21044b5f2f8c9198faa7c917a414687943873e9f
alexspector123 <alexspector8766@gmail.com>
2025-07-28 16:58:52 +0700

    e2e testing

--- src/songs/songs.controller.spec.ts
+++ src/songs/songs.controller.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SongsController } from './songs.controller';
+import { SongsService } from './songs.service';
+import { CreateSongDTO } from './dto/create-song-dto';
+import { UpdateSongDTO } from './dto/update-song-dto';
 
-describe('SongsController', () => {
+describe('SongController', () => {
   let controller: SongsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SongsController],
+      providers: [
+        SongsService,
+        {
+          provide: SongsService,
+          useValue: {
+            getSongs: jest
+              .fn()
+              .mockResolvedValue([{ id: 1, title: 'Dancing Feat' }]),
+            getSong: jest.fn().mockImplementation((id: string) => {
+              return Promise.resolve({ id: id, title: 'Dancing' });
+            }),
+            createSong: jest
+              .fn()
+              .mockImplementation((createSongDTO: CreateSongDTO) => {
+                return Promise.resolve({ id: 'a uuid', ...createSongDTO });
+              }),
+            updateSong: jest
+              .fn()
+              .mockImplementation((updateSongDTO: UpdateSongDTO) => {
+                return Promise.resolve({ affected: 1 });
+              }),
+
+            deleteSong: jest.fn().mockImplementation((id: string) => {
+              return Promise.resolve({ affected: 1 });
+            }),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<SongsController>(SongsController);
@@ -14,5 +45,51 @@ describe('SongsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getSongs', () => {
+    it('should fetch all the songs', async () => {
+      const songs = await controller.findAll();
+      expect(songs).toEqual([{ id: 1, title: 'Dancing Feat' }]);
+    });
+  });
+
+  describe('getSong by id', () => {
+    it('should give me the song by id', async () => {
+      const song = await controller.findOne(123);
+      expect(song.id).toBe('a uuid');
+    });
+  });
+
+  describe('createSong', () => {
+    it('should create a new song', async () => {
+      const createSong: CreateSongDTO = {
+        title: 'Runaway',
+      };
+      const song = await controller.create(createSong);
+      expect(song.title).toBe('Runaway');
+      expect(song).toEqual({ id: 'a uuid', title: 'Runaway' });
+    });
+  });
+
+  describe('updateSong', () => {
+    it('should update the song DTO', async () => {
+      const updatesongDTO: UpdateSongDTO = {
+        title: 'Animals',
+      };
+      const updateResults = await controller.update(
+        123,
+        updatesongDTO,
+      );
+      expect(updateResults).toBeDefined();
+      expect(updateResults.affected).toBe(1);
+    });
+  });
+
+  describe('deleteSong', () => {
+    it('should delete the song', async () => {
+      const deleteResult = await controller.delete(123);
+      expect(deleteResult.affected).toBe(1);
+    });
   });
 });
--- src/songs/songs.service.spec.ts
+++ src/songs/songs.service.spec.ts
@@ -1,18 +1,89 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SongsService } from './songs.service';
+import { Song } from './song.entity';
+import { FindOneOptions, Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreateSongDTO } from './dto/create-song-dto';
+import { UpdateSongDTO } from './dto/update-song-dto';
 
-describe('SongsService', () => {
+describe('SongService', () => {
   let service: SongsService;
+  let repo: Repository<Song>;
+
+  const oneSong = { id: 123, title: 'Lover' };
+  const songArray = [{ id: 456, title: 'Lover' }];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SongsService],
+      providers: [
+        SongsService,
+        {
+          provide: getRepositoryToken(Song),
+          useValue: {
+            find: jest
+              .fn()
+              .mockImplementation(() => Promise.resolve(songArray)),
+            findOneOrFail: jest
+              .fn()
+              .mockImplementation((options: FindOneOptions<Song>) => {
+                return Promise.resolve(oneSong);
+              }),
+            create: jest
+              .fn()
+              .mockImplementation((createSongDTO: CreateSongDTO) => {
+                return Promise.resolve(oneSong);
+              }),
+            save: jest.fn(),
+            update: jest
+              .fn()
+              .mockImplementation(
+                (id: string, updateSongDTO: UpdateSongDTO) => {
+                  return Promise.resolve({ affected: 1 });
+                },
+              ),
+            delete: jest
+              .fn()
+              .mockImplementation((id: string) =>
+                Promise.resolve({ affected: 1 }),
+              ),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<SongsService>(SongsService);
+    repo = module.get<Repository<Song>>(getRepositoryToken(Song));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should give me the song by id', async () => {
+    const song = await service.findOne(123);
+    const repoSpy = jest.spyOn(repo, 'findOneOrFail');
+    expect(song).toEqual(oneSong);
+    expect(repoSpy).toBeCalledWith({ where: { id: 'a uuid' } });
+  });
+
+  it('should create the song', async () => {
+    const song = await service.create({ title: 'Lover' });
+    expect(song).toEqual(oneSong);
+    expect(repo.create).toBeCalledTimes(1);
+    expect(repo.create).toBeCalledWith({ title: 'Lover' });
+  });
+
+  it('should update the song', async () => {
+    const result = await service.update(123, { title: 'Lover' });
+    expect(repo.update).toBeCalledTimes(1);
+    expect(result.affected).toEqual(1);
+  });
+
+  it('should delete the song', async () => {
+    const song = await service.delete(123);
+    const repoSpyOn = jest.spyOn(repo, 'delete');
+    expect(repo.delete).toBeCalledTimes(1);
+    expect(song.affected).toBe(1);
+    expect(repoSpyOn).toBeCalledWith('a uuid');
   });
 });
